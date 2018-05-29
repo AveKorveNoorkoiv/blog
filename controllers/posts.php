837495e5eb0f5e4ab923b2cfcdb4ceeb42c809f6
AveKorveNoorkoiv <ave.korve-noorkoiv@khk.ee>
2018-05-29 16:02:23 +0300

    Rename welcome controller

--- controllers/posts.php
+++ controllers/posts.php
@@ -2,28 +2,24 @@
 class posts extends Controller
 {
     /**
-     * This is a normal action which will be called when user visits /posts/index URL. Since index is the default
-     * action name, it may be omitted (URL can be /posts). Since posts is by default the default controller, it may
+     * This is a normal action which will be called when user visits /welcome/index URL. Since index is the default
+     * action name, it may be omitted (URL can be /welcome). Since welcome is by default the default controller, it may
      * be omitted (URL can be /). After calling the action, a view from views/controller-name/controller-name_action-name.php
      * is loaded (it must exist, unless the function ends with stop() call.
      */
     function index()
     {
-        $this->posts = get_all("SELECT * FROM post");
-        $_tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag");
-        foreach($_tags as $tag){
-            $this->tags[$tag['post_id']][] = $tag;
-        }
+        $this->posts = get_all("SELECT * FROM posts");
     }
     /**
-     * Single-view for the blog posts
+     * Post view
+     *
      */
     function view()
     {
         $post_id = $this->params[0];
-        $this->post = get_first("SELECT * FROM post NATURAL JOIN users WHERE post_id='$post_id'");
-        $this->tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag WHERE post_id='$post_id'");
-        $this->comments = get_all("SELECT * FROM post_comments NATURAL JOIN comment WHERE post_id='$post_id'");
+        $this->post= get_first("SELECT * FROM posts NATURAL JOIN users WHERE post_id='$post_id'");
+        $this->tags= get_all("SELECT * FROM post_tags NATURAL JOIN tag WHERE post_id='$post_id'");
     }
     /**
      * This function will only be ran in case of an AJAX request. No view will be attempted to load after this function.
