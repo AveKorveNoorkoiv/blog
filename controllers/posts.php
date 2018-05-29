a490d394dfc62f5634363522ffe4f54fdb23cd27
AveKorveNoorkoiv <ave.korve-noorkoiv@khk.ee>
2018-05-29 15:06:41 +0300

    Post controller inndex action

--- controllers/posts.php
+++ controllers/posts.php
@@ -1,8 +1,6 @@
 <?php namespace App;
-
 class posts extends Controller
 {
-
     /**
      * This is a normal action which will be called when user visits /posts/index URL. Since index is the default
      * action name, it may be omitted (URL can be /posts). Since posts is by default the default controller, it may
@@ -17,7 +15,6 @@ class posts extends Controller
             $this->tags[$tag['post_id']][] = $tag;
         }
     }
-
     /**
      * Single-view for the blog posts
      */
@@ -28,35 +25,25 @@ class posts extends Controller
         $this->tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag WHERE post_id='$post_id'");
         $this->comments = get_all("SELECT * FROM post_comments NATURAL JOIN comment WHERE post_id='$post_id'");
     }
-
     /**
      * This function will only be ran in case of an AJAX request. No view will be attempted to load after this function.
      */
     function AJAX_success()
     {
-
-
         stop(201,'Everything is awesome');
     }
-
     /**
      * This function will only be ran in case of an AJAX request. No view will be attempted to load after this function.
      */
     function AJAX_error()
     {
-
         // Test sending emails
         Mail::send(DEVELOPER_EMAIL, 'test', 'test');
-
         echo "This text comes from the server and will be shown only in development environment for debugging purposes. ";
         echo "Here is a nice exception for you to debug:";
-
         // Generate error for testing
         throw new \Exception('This is a test');
-
-
     }
-
     /**
      * This function will only be ran in case of POST request
      */
